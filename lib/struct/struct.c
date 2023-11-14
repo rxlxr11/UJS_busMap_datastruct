47112d2ccb8b22c68e29bd80f1029295a58a66b5
rxlxr11 <112249010+rxlxr11@users.noreply.github.com>
2023-11-14 10:40:33 +0800

    Update struct.c

--- lib/struct/struct.c
+++ lib/struct/struct.c
@@ -28,19 +28,31 @@ LinkList* createLink(char *s){
 }
 
 //尾插法
-void insert_wayNode(wayNode *n,int id,char *name,char *route){
-	while(n->next!=NULL){
-		n=n->next;
-	}
-	wayNode *x=(wayNode*)malloc(sizeof(wayNode));
-	x->id=id;
-	strcpy(x->name,name);
-	strcpy(x->route,route);
-	x->next=NULL;
-	n->next=x;
+void insert_wayNode(headN *n,int id,char *name,char *route,float distance){
+    wayNode *x=(wayNode*)malloc(sizeof(wayNode));
+    x->id=id;
+    strcpy(x->name,name);
+    strcpy(x->route,route);
+    x->next=NULL;
+    if(n->next!=NULL)
+    {
+        wayNode *t=(wayNode*)malloc(sizeof(wayNode));
+        t = n->next;
+        while(t->next!=NULL){
+            t=t->next;
+        }
+        t->next=x;
+    } else{
+        n->next = x;
+        n->node++;
+        n->distance+=distance;
+    }
+
+
+
 }
 
-void copy_way(wayNode *source,wayNode *wcopy){
+void copy_way(headN *source,headN *wcopy){
     wcopy->next=NULL;
     wayNode *wc=(wayNode*)malloc(sizeof(wayNode));
     wc=source->next;
@@ -50,8 +62,14 @@ void copy_way(wayNode *source,wayNode *wcopy){
     }
 }
 wayNode* createWay() {
-    wayNode *n;
-    n = (wayNode*)malloc(sizeof(wayNode));
+    headN *n;
+    n = (headN *)malloc(sizeof(headN ));
+    n->next = NULL;
+    n->distance = 0;
+    n->change = 0;
+    n->price = 0;
+    n->time = 0;
+    n->node = 0;
     return n;
 }
 
