a94b1ecd6c8793acbf9efc18cadedcdb4d4095f7
rxlxr11 <112249010+rxlxr11@users.noreply.github.com>
2023-11-20 14:57:32 +0800

    Update struct.c

--- lib/struct/struct.c
+++ lib/struct/struct.c
@@ -27,51 +27,7 @@ LinkList* createLink(char *s){
 	return L;
 }
 
-//尾插法
-void insert_wayNode(headN *n,int id,char *name,char *route,float distance){
-    wayNode *x=(wayNode*)malloc(sizeof(wayNode));
-    x->id=id;
-    strcpy(x->name,name);
-    strcpy(x->route,route);
-    x->next=NULL;
-    if(n->next!=NULL)
-    {
-        wayNode *t=(wayNode*)malloc(sizeof(wayNode));
-        t = n->next;
-        while(t->next!=NULL){
-            t=t->next;
-        }
-        t->next=x;
-    } else{
-        n->next = x;
 
-    }
-				n->node++;
-    n->distance+=distance;
-
-
-}
-
-void copy_way(headN *source,headN *wcopy){
-    wcopy->next=NULL;
-    wayNode *wc=(wayNode*)malloc(sizeof(wayNode));
-    wc=source->next;
-    while(wc!=NULL){
-        insert_wayNode(wcopy,wc->id,wc->name,wc->route);
-        wc=wc->next;
-    }
-}
-wayNode* createWay() {
-    headN *n;
-    n = (headN *)malloc(sizeof(headN ));
-    n->next = NULL;
-    n->distance = 0;
-    n->change = 0;
-    n->price = 0;
-    n->time = 0;
-    n->node = 0;
-    return n;
-}
 
 Neighbour* find_Neighbour(int id, Point p) {
     Neighbour *n = p.first;
