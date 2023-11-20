bcb1ea70bfbbf36d217efdbf7a6352a625d63414
rxlxr11 <112249010+rxlxr11@users.noreply.github.com>
2023-11-20 14:58:21 +0800

    Update func.h

--- lib/func/func.h
+++ lib/func/func.h
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include <string.h>
 int equal_f(char*,char*);
-int locate(char *,char **);
 int locate_int(int,int a[25],int); 
 int splitComma(char *,char **);
 int findSame(int a[10],int b[10],int c[10],int,int);
